17d135f5af13be8c2aacce40a6d33574dc6c287a
João Oliveira <deoliveira.jvm@gmail.com>
2026-01-16 01:27:50 -0300

    ✨ Persist in localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import App from "@/app";
 import "@/styles.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { QueryClient } from "@tanstack/react-query";
+import {
+  PersistQueryClientProvider,
+  removeOldestQuery,
+} from "@tanstack/react-query-persist-client";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
@@ -9,16 +14,26 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: Infinity,
       gcTime: 1000 * 60 * 60 * 24, // 24 hours
-      // persister
-      // retry
+      // retry only if error is not cors
     },
   },
 });
 
+const storagePersister = createAsyncStoragePersister({
+  storage: localStorage,
+  retry: removeOldestQuery,
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{
+        persister: storagePersister,
+        maxAge: 1000 * 60 * 60 * 24, // 24 hours
+      }}
+    >
       <App />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>
 );
