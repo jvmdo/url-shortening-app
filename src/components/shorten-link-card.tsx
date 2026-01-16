abaa0565092e4c2e1d5c7d2a8fc729f3f6229aaf
João Oliveira <deoliveira.jvm@gmail.com>
2026-01-16 00:54:00 -0300

    ✨ Copy to clipboard

--- src/components/shorten-link-card.tsx
+++ src/components/shorten-link-card.tsx
@@ -1,9 +1,10 @@
+import React from "react";
+
 import Button from "@/components/button";
 import { Separator } from "@base-ui/react";
-import type { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface ShortenLinkCardProps extends ComponentProps<"div"> {
+interface ShortenLinkCardProps extends React.ComponentProps<"div"> {
   href: string;
   shorten: string;
 }
@@ -14,6 +15,34 @@ function ShortenLinkCard({
   className,
   ...delegated
 }: ShortenLinkCardProps) {
+  const [btnText, setBtnText] = React.useState<"copy" | "copied">("copy");
+  const intervalIdRef = React.useRef<number | null>(null);
+
+  const handleCopyToClipboard = async () => {
+    try {
+      if (intervalIdRef.current) {
+        clearInterval(intervalIdRef.current);
+      }
+
+      await navigator.clipboard.writeText(shorten);
+      setBtnText("copied");
+
+      const intervalId = setInterval(() => {
+        setBtnText("copy");
+      }, 5000);
+
+      intervalIdRef.current = intervalId;
+      console.log(intervalId);
+    } catch (err) {
+      // toast
+      // NotAllowedError DOMException
+      // Thrown if writing to the clipboard is not allowed.
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  const isCopied = btnText === "copied";
+
   return (
     <div
       {...delegated}
@@ -22,7 +51,14 @@ function ShortenLinkCard({
       <p>{href}</p>
       <Separator orientation="horizontal" />
       <strong>{shorten}</strong>
-      <Button type="button">Copy</Button>
+      <Button
+        type="button"
+        className="capitalize disabled:bg-primary-dark!"
+        disabled={isCopied}
+        onClick={handleCopyToClipboard}
+      >
+        {btnText}
+      </Button>
     </div>
   );
 }
